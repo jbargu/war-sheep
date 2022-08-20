0110d302934da2c34235bf69ba320110b35c7c09
Piturnah <peterhebden6@gmail.com>
2022-08-20 15:33:23 +0100

    Basic sheep combining

--- src/sheep.rs
+++ src/sheep.rs
@@ -7,7 +7,10 @@ pub struct SheepPlugin;
 
 impl Plugin for SheepPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(spawn_sheep).add_system(select_sheep);
+        app.add_startup_system(init_sheep)
+            .add_system_to_stage(CoreStage::PreUpdate, select_sheep)
+            .add_system(drop_sheep)
+            .add_system(update_sheep);
     }
 }
 
@@ -15,32 +18,50 @@ const X_MAX_POS_OFFSET: f32 = 10.0;
 const Y_MAX_POS_OFFSET: f32 = 6.0;
 const COUNT_INIT_SHEEP: usize = 10;
 
-#[derive(Component)]
-pub struct Sheep;
+#[derive(Component, Default)]
+pub struct Sheep {
+    // In future we can put all the sheep traits here
+    state: u8,
+}
 
-fn spawn_sheep(mut commands: Commands) {
+fn spawn_sheep(commands: &mut Commands, transform: Transform, sheep: Sheep) -> Entity {
+    commands
+        .spawn_bundle(SpriteBundle {
+            transform,
+            sprite: Sprite {
+                color: Color::WHITE,
+                ..default()
+            },
+            ..default()
+        })
+        .insert(sheep)
+        .id()
+}
+
+#[derive(Component)]
+pub struct SheepParent;
+
+fn init_sheep(mut commands: Commands) {
     let mut rng = thread_rng();
 
     let mut sheep = Vec::with_capacity(COUNT_INIT_SHEEP);
     for i in 0..=COUNT_INIT_SHEEP {
+        let new_sheep = spawn_sheep(
+            &mut commands,
+            Transform {
+                translation: Vec3::new(
+                    rng.gen_range(-X_MAX_POS_OFFSET..=X_MAX_POS_OFFSET),
+                    rng.gen_range(-Y_MAX_POS_OFFSET..=Y_MAX_POS_OFFSET),
+                    0.0,
+                ),
+                ..default()
+            },
+            Sheep::default(),
+        );
+
         sheep.push(
             commands
-                .spawn_bundle(SpriteBundle {
-                    transform: Transform {
-                        translation: Vec3::new(
-                            rng.gen_range(-X_MAX_POS_OFFSET..=X_MAX_POS_OFFSET),
-                            rng.gen_range(-Y_MAX_POS_OFFSET..=Y_MAX_POS_OFFSET),
-                            0.0,
-                        ),
-                        ..default()
-                    },
-                    sprite: Sprite {
-                        color: Color::rgb(1.0, 1.0, 1.0),
-                        ..default()
-                    },
-                    ..default()
-                })
-                .insert(Sheep)
+                .entity(new_sheep)
                 .insert(Name::from(format!("Sheep_{i}")))
                 .id(),
         );
@@ -48,6 +69,7 @@ fn spawn_sheep(mut commands: Commands) {
 
     commands
         .spawn_bundle(SpatialBundle::default())
+        .insert(SheepParent)
         .insert(Name::from("SheepParent"))
         .push_children(&sheep);
 }
@@ -88,6 +110,56 @@ fn select_sheep(
     } else if mouse_btn.just_released(MouseButton::Left) {
         for (sheep, _) in &sheep_q {
             commands.entity(sheep).remove::<Drag>();
+        }
+    }
+}
+
+fn drop_sheep(
+    mut commands: Commands,
+    dropped: RemovedComponents<Drag>,
+    sheep: Query<(Entity, &Sheep, &Transform)>,
+    sheep_parent: Query<Entity, With<SheepParent>>,
+) {
+    for drop in dropped.iter() {
+        if let Ok((_, sheep_component, dropped_transform)) = sheep.get(drop) {
+            if let Some((collided, _, collided_transform)) = sheep
+                .iter()
+                .filter(|(_, _, transform)| {
+                    transform
+                        .translation
+                        .distance(dropped_transform.translation)
+                        <= transform.scale.x
+                })
+                .filter(|(entity, _, _)| entity.id() != drop.id())
+                .next()
+            {
+                commands.entity(drop).despawn_recursive();
+                commands.entity(collided).despawn_recursive();
+
+                let new_sheep = spawn_sheep(
+                    &mut commands,
+                    *collided_transform,
+                    Sheep {
+                        // In here we would have the actual trait mutation / combination rather
+                        // than just incrementing a state value
+                        state: sheep_component.state + 1,
+                    },
+                );
+
+                commands.entity(sheep_parent.single()).add_child(new_sheep);
+            }
+        }
+    }
+}
+
+fn update_sheep(mut q: Query<(&mut Sprite, &Sheep), Changed<Sheep>>) {
+    for (mut sprite, sheep) in q.iter_mut() {
+        sprite.color = match sheep.state {
+            0 => Color::rgb(1.0, 1.0, 1.0),
+            1 => Color::rgb(1.0, 0.0, 0.0),
+            2 => Color::rgb(0.0, 1.0, 0.0),
+            3 => Color::rgb(0.0, 0.0, 1.0),
+            _ => Color::PURPLE,
         }
     }
 }
